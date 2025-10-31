716803b0fb051fcc6496d517a39925e3715cc6e9
alsexandersulaberidze <alexander.sulaberidze@azry.com>
2025-10-31 11:56:47 +0400

    problem 2 & problem 3 fix

--- HW1/Problem2.py
+++ HW1/Problem2.py
@@ -1,8 +1,11 @@
 number = int(input("Enter number: "))
 
-divisors = []
-for i in range(1, number + 1):
-    if number % i == 0:
-        divisors.append(i)
+if number <= 0 or number >= 1000:
+    print("Please enter a natural number less than 1000.")
+else:
+    divisors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            divisors.append(i)
 
-print(" ".join(map(str, divisors)))
+    print(" ".join(map(str, divisors)))
--- HW1/Problem3.py
+++ HW1/Problem3.py
@@ -1,13 +1,15 @@
 n = int(input("Enter number: "))
 
-fibonacci = []
+if n <= 0 or n >= 100:
+    print("Please enter a natural number less than 100.")
+else:
+    fibonacci = []
+    for i in range(n):
+        if i == 0:
+            fibonacci.append(0)
+        elif i == 1:
+            fibonacci.append(1)
+        else:
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
 
-for i in range(n):
-    if i == 0:
-        fibonacci.append(0)
-    elif i == 1:
-        fibonacci.append(1)
-    else:
-        fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
-
-print(fibonacci)
+    print(fibonacci)
